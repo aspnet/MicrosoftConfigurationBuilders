37a9908e36654e0eaaabb9588e80a6ff37d281ab
Steve Molloy <smolloy@microsoft.com>
2018-05-01 11:12:43 -0700

    Create ID and link secrets.xml in 'Properties' via Install.ps1

---
+++ src/packages/ConfigurationBuilders.UserSecrets.nupkg/tools/Net471/Uninstall.ps1
@@ -0,0 +1,4 @@
+﻿# Copyright (c) .NET Foundation. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+param($installPath, $toolsPath, $package, $project)
