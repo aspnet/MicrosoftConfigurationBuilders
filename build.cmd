9eedc1b82cc6323a01d66429f5d8712c59693998
Steve Molloy <smolloy@microsoft.com>
2018-06-01 13:07:39 -0700

    Cleaning up solution files.

--- build.cmd
+++ build.cmd
@@ -3,47 +3,15 @@
 setlocal
 set EnableNuGetPackageRestore=true
 
-REM msbuild will compile in Debug mode by default. The nuget packages project is Release by default.
-REM Long story short... be explicit. Don't use defaults.
+set MSBUILDEXE=msbuild.exe
+
 set cfgOption=/p:Configuration=Release
 REM set cfgOption=/p:Configuration=Debug
 REM set cfgOption=/p:Configuration=Debug;Release
 
-set logOptions=/flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
+REM set logOptions=/v:d /flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
+set logOptions=/v:diag /flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
 
-set MSBUILDEXE=
-
-if exist "%SystemDrive%\Program Files (x86)\MSBuild\15.0\Bin\MSBuild.exe" (
-	set MSBUILDEXE="%SystemDrive%\Program Files (x86)\MSBuild\15.0\Bin\MSBuild.exe"
-	GOTO BUILD
-)
-
-if exist "%SystemDrive%\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" (
-	set MSBUILDEXE="%SystemDrive%\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
-	GOTO BUILD
-)
-
-if exist "%SystemDrive%\Program Files (x86)\Microsoft Visual Studio\Preview\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" (
-	set MSBUILDEXE="%SystemDrive%\Program Files (x86)\Microsoft Visual Studio\Preview\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
-	GOTO BUILD
-)
-
-if exist "%SystemDrive%\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" (
-	set MSBUILDEXE="%SystemDrive%\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
-	GOTO BUILD
-)
-
-if exist "%SystemDrive%\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" (
-	set MSBUILDEXE="%SystemDrive%\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
-	GOTO BUILD
-)
-
-if not defined MSBUILDEXE (
-	set MSBUILDEXE="%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
-)
-
-:BUILD
-	REM %MSBUILDEXE% "%~dp0\MicrosoftConfigurationBuilders.msbuild" %cfgOption% %logOptions% /v:d /maxcpucount /nodeReuse:false %*
-	%MSBUILDEXE% "%~dp0\MicrosoftConfigurationBuilders.msbuild" %cfgOption% %logOptions% /v:diag /maxcpucount /nodeReuse:false %*
+%MSBUILDEXE% "%~dp0\MicrosoftConfigurationBuilders.msbuild" %cfgOption% %logOptions% /maxcpucount /nodeReuse:false %*
 
 endlocal
--- clean.cmd
+++ clean.cmd
@@ -2,44 +2,18 @@
 
 setlocal
 
-set logOptions=/flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
+set MSBUILDEXE=msbuild.exe
 
-set MSBUILDEXE=
+REM set logOptions=/v:d /flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
+set logOptions=/v:diag /flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
 
-if exist "%SystemDrive%\Program Files (x86)\MSBuild\15.0\Bin\MSBuild.exe" (
-	set MSBUILDEXE="%SystemDrive%\Program Files (x86)\MSBuild\15.0\Bin\MSBuild.exe"
-	GOTO BUILD
-)
+set cfgOption=/p:Configuration=Release
+REM set cfgOption=/p:Configuration=Debug
+REM set cfgOption=/p:Configuration=Debug;Release
 
-if exist "%SystemDrive%\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" (
-	set MSBUILDEXE="%SystemDrive%\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
-	GOTO BUILD
-)
-
-if exist "%SystemDrive%\Program Files (x86)\Microsoft Visual Studio\Preview\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" (
-	set MSBUILDEXE="%SystemDrive%\Program Files (x86)\Microsoft Visual Studio\Preview\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
-	GOTO BUILD
-)
-
-if exist "%SystemDrive%\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" (
-	set MSBUILDEXE="%SystemDrive%\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
-	GOTO BUILD
-)
-
-if exist "%SystemDrive%\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" (
-	set MSBUILDEXE="%SystemDrive%\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
-	GOTO BUILD
-)
-
-if not defined MSBUILDEXE (
-	set MSBUILDEXE="%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
-)
-
-:BUILD
-	REM %MSBUILDEXE% "%~dp0\MicrosoftConfigurationBuilders.msbuild" /t:Clean %logOptions% /v:d /maxcpucount /nodeReuse:false %*
-	%MSBUILDEXE% "%~dp0\MicrosoftConfigurationBuilders.msbuild" /t:Clean %logOptions% /v:diag /maxcpucount /nodeReuse:false %*
-	del /F msbuild.log
-	del /F msbuild.wrn
-	del /F msbuild.err
+%MSBUILDEXE% "%~dp0\MicrosoftConfigurationBuilders.msbuild" /t:Clean %cfgOption% %logOptions% /maxcpucount /nodeReuse:false %*
+del /F msbuild.log
+del /F msbuild.wrn
+del /F msbuild.err
 
 endlocal
