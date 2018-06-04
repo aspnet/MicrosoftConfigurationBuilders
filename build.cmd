23563856728357b199850b3906aa47fe709a1741
Steve Molloy <smolloy@microsoft.com>
2018-06-04 12:12:45 -0700

    More solution cleanup.

--- build.cmd
+++ build.cmd
@@ -10,8 +10,8 @@ REM set cfgOption=/p:Configuration=Debug
 REM set cfgOption=/p:Configuration=Debug;Release
 if not "%1"=="" set cfgOption=/p:Configuration=
 
-REM set logOptions=/v:d /flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
-set logOptions=/v:diag /flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
+set logOptions=/v:n /flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
+REM set logOptions=/v:diag /flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
 
 %MSBUILDEXE% "%~dp0\MicrosoftConfigurationBuilders.msbuild" %logOptions% /maxcpucount /nodeReuse:false %cfgOption%%*
 
--- clean.cmd
+++ clean.cmd
@@ -9,8 +9,8 @@ REM set cfgOption=/p:Configuration=Debug
 REM set cfgOption=/p:Configuration=Debug;Release
 if not "%1"=="" set cfgOption=/p:Configuration=
 
-REM set logOptions=/v:d /flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
-set logOptions=/v:diag /flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
+set logOptions=/v:n /flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
+REM set logOptions=/v:diag /flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
 
 %MSBUILDEXE% "%~dp0\MicrosoftConfigurationBuilders.msbuild" /t:Clean %logOptions% /maxcpucount /nodeReuse:false %cfgOption%%*
 del /F msbuild.log
