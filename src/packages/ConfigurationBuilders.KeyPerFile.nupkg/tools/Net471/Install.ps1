c66672c8186f53dbbb5da9a339ca7ff3c346d94c
Steve Molloy <smolloy@microsoft.com>
2018-06-22 17:56:11 -0700

    Add KeyPerFile config builder, similar in concept/behavior as the KeyPerFile config source in .Net Core.

---
+++ src/packages/ConfigurationBuilders.KeyPerFile.nupkg/tools/Net471/Install.ps1
@@ -0,0 +1,21 @@
+﻿# Copyright (c) .NET Foundation. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+param($installPath, $toolsPath, $package, $project)
+
+. "$PSScriptRoot\KeyValueConfigBuildersCommon.ps1"
+
+##### Describe the KeyPerFile config builder #####
+$keyPerFileConfigBuilder = [BuilderDescription]@{
+	TypeName="$typeName$";
+	Assembly="$assemblyName$";
+	Version="$assemblyVersion$";
+	DefaultName="KeyPerFile";
+	AllowedParameters=@( $keyValueCommonParameters +
+		[ParameterDescription]@{ Name="directoryPath"; IsRequired=$true; DefaultValue="[PathToSourceDirectory]" },
+		[ParameterDescription]@{ Name="keyDelimiter"; IsRequired=$false },
+		[ParameterDescription]@{ Name="ignorePrefix"; IsRequired=$false },
+		[ParameterDescription]@{ Name="optional"; IsRequired=$false });
+}
+
+CommonInstall $keyPerFileConfigBuilder
