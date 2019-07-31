320e893bc4b0d7447c9fbe785f7e710dceb3c0db
Nir Weber <webernir@tipranks.com>
2019-07-31 08:39:59 +0300

    Adds WorkerRoleConfigurationBuilder

---
+++ WorkerRoleServiceConfiguration/WorkerRoleConfigurationBuilder.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace Microsoft.Configuration.ConfigurationBuilders
+{
+    public class WorkerRoleConfigurationBuilder : KeyValueConfigBuilder
+    {
+        public override string GetValue(string key)
+        {
+            return null;
+        }
+
+        public override ICollection<KeyValuePair<string, string>> GetAllValues(string prefix)
+        {
+            return new Dictionary<string, string>();
+        }
+
+        public override ConfigurationSection ProcessConfigurationSection(ConfigurationSection configSection)
+        {
+            var res = base.ProcessConfigurationSection(configSection);
+
+            switch (configSection)
+            {
+                case ConnectionStringsSection connectionStringsSection:
+                    OverrideConnectionStringsWithCloud(connectionStringsSection);
+                    break;
+
+                case AppSettingsSection appSettingsSection:
+                    OverrideAppSettingsWithCloud(appSettingsSection);
+                    break;
+            }
+
+            return res;
+        }
+
+        private void OverrideConnectionStringsWithCloud(ConnectionStringsSection section)
+        {
+            foreach (ConnectionStringSettings conString in section.ConnectionStrings)
+            {
+                conString.ConnectionString = ResolveAppSettingValue(key: conString.Name, defaultValue: conString.ConnectionString);
+            }
+        }
+
+        private void OverrideAppSettingsWithCloud(AppSettingsSection section)
+        {
+            foreach (KeyValueConfigurationElement appSetting in section.Settings)
+            {
+                appSetting.Value = ResolveAppSettingValue(key: appSetting.Key, defaultValue: appSetting.Value);
+            }
+        }
+
+
+        private string ResolveAppSettingValue(string key, string defaultValue)
+        {
+            var hasSettingOnCloud = TryGetCloudAppSetting(key, out var cloudSettingValue);
+            return (hasSettingOnCloud) ? cloudSettingValue : defaultValue;
+        }
+
+        private bool TryGetCloudAppSetting(string key, out string settingValue)
+        {
+            try
+            {
+                settingValue = RoleEnvironment.GetConfigurationSettingValue(key);
+                return !string.IsNullOrWhiteSpace(settingValue);
+            }
+            catch (Exception)
+            {
+                // RoleEnvironment.GetConfigurationSettingValue throws if no value, so we want to trace this error and move on. 
+                settingValue = null;
+                return false;
+            }
+        }
+    }
+}
---
+++ test/Microsoft.Configuration.ConfigurationBuilders.Test/WorkerRoleConfigurationBuilderTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Configuration.ConfigurationBuilders;
+using Xunit;
+
+
+namespace Test
+{
+    public class WorkerRoleConfigurationBuilderTests
+    {
+        public WorkerRoleConfigurationBuilderTests()
+        {
+            // Populate the filesystem with key/value pairs that are needed for common tests
+        }
+
+        // ======================================================================
+        //   KeyPerFile parameters
+        // ======================================================================
+        //    - See Parameters section of BaseTests for examples
+        // directoryPath
+        // keyDelimiter
+        // ignorePrefix
+        // optional
+
+
+        // ======================================================================
+        //   CommonBuilderTests
+        // ======================================================================
+        [Fact]
+        public void WorkerRoleConfigurationBuilder_GetValue()
+        {
+        }
+
+        [Fact]
+        public void WorkerRoleConfigurationBuilder_GetAllValues()
+        {
+        }
+
+        // ======================================================================
+        //   Errors
+        // ======================================================================
+        // Make sure various expected exceptions from KeyPerFile contain the name of the builder
+        // No file AND no ID specified
+        // File and/or ID specified, but can't be found and optional = false
+    }
+}
