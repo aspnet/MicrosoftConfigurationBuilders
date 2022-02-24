79b3e9a62238ae293ec237bc48896619a5eadb83
Steve Molloy <smolloy@microsoft.com>
2022-02-23 23:37:24 -0800

    Tweak MapPath slightly and adjust MapPath tests for new expectations.

--- test/Microsoft.Configuration.ConfigurationBuilders.Test/UtilsTests.cs
+++ test/Microsoft.Configuration.ConfigurationBuilders.Test/UtilsTests.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using Microsoft.Configuration.ConfigurationBuilders;
+using System;
 using System.IO;
 using System.Reflection;
-using Microsoft.Configuration.ConfigurationBuilders;
 using Xunit;
 
 namespace Test
@@ -11,20 +11,21 @@ namespace Test
         [Fact]
         public void Utils_MapPath()
         {
-            // Absolute paths
-            Assert.Equal(Utils.MapPath(@"C:\Windows\System32"), Path.GetFullPath(@"C:\Windows\System32"));
-            Assert.Equal(Utils.MapPath(@"/Windows"), Path.GetFullPath(@"/Windows"));
+            // Rooted paths don't change
+            Assert.Equal(@"C:\Windows\System32", Utils.MapPath(@"C:\Windows\System32"));
+            Assert.Equal(@"/Windows", Utils.MapPath(@"/Windows"));
+            Assert.Equal(@"\Windows", Utils.MapPath(@"\Windows"));
 
             // Relative paths
-            Assert.Equal(Utils.MapPath(@"foo\bar\baz"), Path.GetFullPath(@"foo\bar\baz"));
-            Assert.Equal(Utils.MapPath(@"..\foo\..\baz"), Path.GetFullPath(@"..\foo\..\baz"));
+            Assert.Equal(Path.GetFullPath(@"foo\bar\baz"), Utils.MapPath(@"foo\bar\baz"));
+            Assert.Equal(Path.GetFullPath(@"..\foo\..\baz"), Utils.MapPath(@"..\foo\..\baz"));
 
             // Home-relative paths
             string baseDir = AppDomain.CurrentDomain.BaseDirectory;
-            Assert.Equal(Utils.MapPath(@"~\"), baseDir);
-            Assert.Equal(Utils.MapPath(@"~/"), baseDir);
-            Assert.Equal(Utils.MapPath(@"~/hello"), Path.Combine(baseDir, @"hello"));
-            Assert.Equal(Utils.MapPath(@"~\good-bye\"), Path.Combine(baseDir, @"good-bye\"));
+            Assert.Equal(baseDir, Utils.MapPath(@"~\"));
+            Assert.Equal(baseDir, Utils.MapPath(@"~/"));
+            Assert.Equal(Path.Combine(baseDir, @"hello"), Utils.MapPath(@"~/hello"));
+            Assert.Equal(Path.Combine(baseDir, @"good-bye\"), Utils.MapPath(@"~\good-bye\"));
         }
 
         [Fact]
